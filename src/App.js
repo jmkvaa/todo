bf468c8c3597ecbaff4f46956f5b0ae4bd365202
jmkvaa <saadataltynbekova38@gmail.com>
2023-03-13 12:10:06 +0600

    started

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css'
+import Header from './components/Header/Header';
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+
+function App() {
+
+  const[todo, setTodo] = useState([
+    {
+      id: 1,
+      title: 'first todo',
+      status: true
+    },
+    {
+      id: 2,
+      title: 'second todo',
+      status: true
+    },
+    {
+      id: 3,
+      title: 'third todo',
+      status: true
+    },
+    
+    
+ ])
+
+ return (
+    <Container>
+      <Header/>
+      <AddTodo todo={todo} setTodo={setTodo}/>
+      <TodoList todo={todo} setTodo={setTodo}/>
+    </Container>
+  );
+}
+
+export default App;
